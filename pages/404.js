5d5edec0fdcee6393f0e790cba7eb03521300bba
ins1d30ut <nfteebsv@protonmail.com>
2022-11-03 01:10:08 -0400

    Commit jsconfig.json

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import { FaExclamationTriangle } from 'react-icons';
+import Link from 'next/link';
+import Layout from '@/components/Layout';
+import styles from '@/styles/404.module.css';
+
+export default function NotFoundPage() {
+  return (
+    <Layout title='Page Not Found'>
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationTriangle />
+        </h1>
+        <h4>Sorry, there is nothing here</h4>
+        <Link href='/'>Go Back Home</Link>
+      </div>
+    </Layout>
+  );
+}
