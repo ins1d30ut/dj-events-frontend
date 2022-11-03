cc0a25986d723a1e5ca9e65e1c7d493716ea07a2
ins1d30ut <nfteebsv@protonmail.com>
2022-11-03 01:44:23 -0400

    commit api demo w slug

--- components/Showcase.js
+++ components/Showcase.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import styles from '@/styles/Showcase.module.css';
 
 export default function Showcase() {
-  return <div>Showcase</div>;
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to the party</h1>
+      <h2>Find the hottest DJ events</h2>
+    </div>
+  );
 }
